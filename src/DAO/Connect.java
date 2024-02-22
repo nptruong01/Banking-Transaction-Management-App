42e2814947843f1e352730dc15cec7ad58befbd7
NguyenPhiTruong01 <113322089+nptruong01@users.noreply.github.com>
2024-02-22 15:54:52 +0700

    Connect.java

--- src/DAO/Connect.java
+++ src/DAO/Connect.java
@@ -19,14 +19,9 @@ public class Connect {
         try
         {
            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-           /* String connectionurl=
-                   "jdbc:sqlserver://localhost:1433; databaseName=quanlybanhang02;integratedSecurity=true;" ;
-            con=DriverManager.getConnection(connectionurl);
-            //hoac
-*/
           
-            String connectionurl= "jdbc:sqlserver://localhost:1434; databaseName=bank;" ;
-            con=DriverManager.getConnection(connectionurl,"sa","123456");
+          
+            
         }
            catch(Exception ex)
             {
